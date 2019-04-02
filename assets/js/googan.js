198e2a63f586f8fc41b0c78acfeb5992b43eb616
Samuel Gaucher <smlgchr@gmail.com>
2019-04-02 15:39:13 +0200

    Cookie privacy opt out implemented (I)

---
+++ assets/js/googan.js
@@ -0,0 +1,43 @@
+
+// This line must be there in the <head> I suppose but it benign, and doesn't constitute a cookie itself.
+// <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135626636-1"></script>
+
+// This is what Google wants us to place first thing in the <head> of each page
+window.dataLayer = window.dataLayer || [];
+// Now I'm writing this line which allows to disabe the tracking, when the value is set to true. I somehow have to pass the value from
+// the decision made in the banner
+window['ga-disable-UA-135626636-1'] = getCookie("gprivacy"); // Disable/Enable GA
+
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-135626636-1');
+
+
+// I will then execute this script in the <head> just after the initial line, as required.
+
+
+
+// To go check if a cookie exists, and if yes, what is its value
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) { 
+    	if (c.substring(name.length, c.length) == "false"){
+    		alert("You cliked Ok! Cookies enabled.");
+    		return false; // if user clicked "Ok!", cookie was set to false, so here enable GA
+    	}
+    	else{
+    		alert("You cliked x! Cookies disabled.");
+    		return true; // if cookie exists and user clicked "x", and set to true and disable GA
+    	}
+    }
+  }
+  alert("Cookie doesn't exist yet.");
+  return true; // if cookie doesn't exist, return true (disable GA)
+}
---
+++ assets/js/privacy.js
@@ -0,0 +1,68 @@
+
+// make if condition involving verifying the existence of the gprivacy cookie. If it exists, don't show up the banner
+
+$(document).ready(function(){
+
+	if (getCookie("gprivacy") == "cookie not found") { // Then show the banner
+	    setTimeout(function () {
+	        $("#cookieConsent").fadeIn(200);
+	     }, 1000);
+	    $(".cookieConsentOK").click(function() {
+	        $("#cookieConsent").fadeOut(200);location.reload();setCookie("gprivacy", "false", 1); //don't need the reload I think
+	    }); 
+	    $("#closeCookieConsent").click(function() {
+	        $("#cookieConsent").fadeOut(200);setCookie("gprivacy", "true", 1) // hihi, only 30 days
+	    }); 
+	    //var user = prompt("Please enter your name:", "");
+	    //alert("Hello! I am an alert box!!" + getCookie("gprivacy")+ "Cool");
+	}
+	else{
+		//alert("The cookie value is: " + getCookie("gprivacy"));
+		return 0;
+	}
+}); 
+
+
+
+
+// $(document).ready(function(){  
+//     setTimeout(function () {
+//         $("#cookieConsent").fadeIn(200);
+//      }, 1000);
+//     $(".cookieConsentOK").click(function() {
+//         $("#cookieConsent").fadeOut(200);location.reload();//setCookie("gprivacy", false, 365); //don't need the reload I think
+//     }); 
+//     $("#closeCookieConsent").click(function() {
+//         $("#cookieConsent").fadeOut(200);//setCookie("gprivacy", true, 30) // hihi, only 30 days
+//     }); 
+// }); 
+
+
+
+// To set the cookie saying you accepted the cookies or not (https://www.w3schools.com/js/js_cookies.asp)
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires;// + ";path=/";
+  alert("Cookie was set to: " + cvalue)
+}
+
+
+// To go check if a cookie exists, and if yes, what is its value
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  //return alert("c.substring(name.length, c.length)");
+  return "cookie not found";
+}
